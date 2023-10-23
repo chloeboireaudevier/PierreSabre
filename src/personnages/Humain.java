8a211b952f64119f7e69cd8d2f8089900b970b96
chloeboireaudevier <cbd65@hotmail.fr>
2023-10-23 18:45:18 +0200

    création de la classe Humain

--- src/personnages/Humain.java
+++ src/personnages/Humain.java
@@ -29,9 +29,25 @@ public class Humain {
 		this.argent -= perte;
 	}
 	
+	public void direBonjour() {
+		parler("Bonjour ! Je m'appelle "+this.nom+" et j'aime boire du "+this.boissonFavorite+" .");
+		
+	}
 	
+	public void boire() {
+		parler("Mmmm, un bon verre de "+this.boissonFavorite+" ! GLOUPS !");
+		
+	}
 	
-	
+	public void acheter(String bien, int prix) {
+		if (this.argent >= prix) {
+			parler("J'ai "+this.argent+" sous en poche. Je vais pouvoir m'offrir "+bien+" à "+prix+" sous");
+			this.argent -= prix;
+		}else {
+			parler("Je n'ai plus que "+this.argent+" sous en poche. Je ne peux même pas m'offrir "+bien+" à "+prix+" sous");
+		}
+		
+	}
 	
 	
 	
