674c7e3c8d5d8d8d100a7ad1267dbfa72e1292c7
chloeboireaudevier <cbd65@hotmail.fr>
2023-11-13 16:23:00 +0100

    Les humains ont des connaissances

--- src/personnages/Humain.java
+++ src/personnages/Humain.java
@@ -1,9 +1,12 @@
 package personnages;
 
 public class Humain {
+	private final int nb_connaissances_max = 30;
 	private String nom;
 	private String boissonFavorite;
 	private int argent;
+	protected int nbConnaissance = 0;
+	protected Humain[] memoire = new Humain[nb_connaissances_max];
 	
 	public Humain(String nom, String boissonFavorite, int argent) {
 		this.nom = nom;
@@ -50,5 +53,47 @@ public class Humain {
 	
 	protected void parler(String texte) {
 		System.out.println("("+this.getNom()+") - "+texte);
+	}
+	
+	public Humain faireConnaissanceAvec(Humain rencontre) {
+		this.direBonjour();
+		rencontre.repondre(this);
+		memoriser(rencontre);
+		return rencontre;
+	}
+	
+	private Humain memoriser(Humain rencontre) {
+		if (this.nbConnaissance < nb_connaissances_max) {
+		this.memoire[this.nbConnaissance] = rencontre;
+		this.nbConnaissance +=1;
+		}else {
+			for (int i = 1 ; i < nb_connaissances_max ; i++) {
+				this.memoire[i-1] = this.memoire[i];
+			}
+			this.memoire[nb_connaissances_max-1] = rencontre;
+		}
+		return rencontre;
+	}
+	
+	private Humain repondre(Humain rencontre) {
+		this.direBonjour();
+		memoriser(rencontre);
+		return rencontre;
+	}
+	
+	public void listerConnaissance() {
+		if (this.nbConnaissance == 0) {
+			this.parler("Je ne connais personne");
+		} else {
+			String chaine = "Je connais beaucoup de monde dont : ";
+			for (int i = 0; i< this.nbConnaissance; i++) {
+				chaine += this.memoire[i].getNom();
+				if ( i != this.nbConnaissance - 1) {
+					chaine +=",";
+				}
+				chaine += " ";
+			}
+			parler(chaine);
+		}
 	}
 }
